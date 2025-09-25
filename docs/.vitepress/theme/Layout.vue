e8acc3126ec8ab973e1a51ee63b46fe19d7c6169
omar-dulaimi <o.m.dulaimi@gmail.com>
2025-09-25 08:11:57 +0300

    docs(theme): place footer in doc-after for docs and layout-bottom for home\n\n- Ensures footer starts after sidenav on docs pages\n- Removes margin hacks; rely on VitePress content grid\n

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,15 +1,21 @@
 <template>
   <Layout>
+    <template #doc-after>
+      <Footer v-if="!isHome" />
+    </template>
     <template #layout-bottom>
-      <Footer />
+      <Footer v-if="isHome" />
     </template>
   </Layout>
-
 </template>
 
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
 import Footer from './components/Footer.vue'
+import { useData } from 'vitepress'
+import { computed } from 'vue'
 
 const { Layout } = DefaultTheme
+const { frontmatter } = useData()
+const isHome = computed(() => frontmatter.value.layout === 'home')
 </script>
